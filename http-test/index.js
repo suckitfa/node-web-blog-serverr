84431243f5cf2e615edce24b53b1b8502aabde47
Bob Tang <254202042@qq.com>
2022-09-02 21:44:46 +0800

    post和get的综合案例

--- http-test/index.js
+++ http-test/index.js
@@ -1,21 +1,59 @@
+// const http = require('http')
+// const querystring = require('querystring')
+// const server = http.createServer((req, res) => {
+//     if (req.method === 'POST') {
+//         console.log('content-type = ', req.headers['content-type']);
+//         let postData = ''
+//         req.on('data', chunk => {
+//             postData += chunk.toString();
+//         })
+//         req.on('end', () => {
+//             console.log(postData)
+//             res.end('Hello wolrd!')
+//         })
+//     } else {
+//         console.log(req.method)
+//         console.log(req.url)
+//         req.query = querystring.parse(req.url.split('?')[1]);
+//         res.end(JSON.stringify(req.query))
+//     }
+// })
+
+// server.listen(8000, () => {
+//     console.log('http://localhost:8000')
+// })
+
 const http = require('http')
 const querystring = require('querystring')
 const server = http.createServer((req, res) => {
-    if (req.method === 'POST') {
-        console.log('content-type = ', req.headers['content-type']);
+    const {
+        method,
+        url,
+    } = req;
+    const path = url.split('?')[0];
+    const query = querystring.decode(url.split('?')[1]);
+    // 设置返回格式json, 返回的永远是一个字符串或者二进制
+    res.setHeader('content-type', 'application/json');
+    // 返回数据
+    const resData = {
+        method,
+        url,
+        path,
+        query
+    }
+    if (method === 'GET') {
+        res.end(JSON.stringify(resData))
+    }
+
+    if (method === 'POST') {
         let postData = ''
         req.on('data', chunk => {
-            postData += chunk.toString();
+            postData += chunk.toString()
         })
+
         req.on('end', () => {
-            console.log(postData)
-            res.end('Hello wolrd!')
+            res.end(JSON.stringify({...resData, postData }));
         })
-    } else {
-        console.log(req.method)
-        console.log(req.url)
-        req.query = querystring.parse(req.url.split('?')[1]);
-        res.end(JSON.stringify(req.query))
     }
 })
 
